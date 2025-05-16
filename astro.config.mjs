be56bd80e13ed3afec4aeec644892dcc7cef5a69
Dom Rodriguez <shymega@shymega.org.uk>
2025-05-16 21:55:04 +0100

    feat: Initial commit for BRN apex site

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+
+import decapCms from 'astro-decap';
+import fulldev from 'fulldev-ui/integration'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [decapCms({
+      cmsConfig: {
+        local_backend: true,
+        backend: { name: "github", repo: "basingstoke-repair-network/apex-site-basingstoke.repair" },
+        media_folder: "public",
+        public_folder: "/",
+      },
+    }),
+    fulldev(),
+    icon(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+
+  legacy: {
+    collections: true,
+  },
+});
