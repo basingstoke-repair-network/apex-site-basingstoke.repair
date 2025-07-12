de991ad8a08f9ccfae58b79413152a4e528aa14f
Dom Rodriguez <shymega@shymega.org.uk>
2025-07-12 17:56:38 +0100

    refactor: Tidy up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2025 Basingstoke Repair Network <dev@basingstoke.repair>
 #
 # SPDX-License-Identifier: CC0-1.0
-
 {
   description = "Astro site for Basingstoke Repair Network";
 
@@ -18,43 +17,43 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
 
-      # Node.js version compatible with Astro
-      nodejs = pkgs.nodejs_20;
-
       # Build the site using Nix-native NPM building
-      site = pkgs.buildNpmPackage {
-        pname = "apex-site-basingstoke-repair";
-        version = "0.1.0";
+      site = let
+        nodejs = pkgs.nodejs_20;
+      in
+        pkgs.buildNpmPackage {
+          pname = "apex-site-basingstoke-repair";
+          version = "0.1.0";
 
-        src = ./.;
+          src = ./.;
 
-        # Hash of the npm dependencies (you may need to update this)
-        npmDepsHash = "sha256-cRd/CUw5yl/CFnkXOQCLPCbI+KkqUNNQYSrwjj0+CX8=";
+          # Hash of the npm dependencies (you may need to update this)
+          npmDepsHash = "sha256-cRd/CUw5yl/CFnkXOQCLPCbI+KkqUNNQYSrwjj0+CX8=";
 
-        npmFlags = ["--legacy-peer-deps"];
+          npmFlags = ["--legacy-peer-deps"];
 
-        # Required for sharp dependency
-        nativeBuildInputs = with pkgs; [
-          python3
-          pkg-config
-          vips
-        ];
+          # Required for sharp dependency
+          nativeBuildInputs = with pkgs; [
+            python3
+            pkg-config
+            vips
+          ];
 
-        buildInputs = with pkgs; [
-          vips
-        ];
+          buildInputs = with pkgs; [
+            vips
+          ];
 
-        # Use the specified Node.js version
-        inherit nodejs;
+          # Use the specified Node.js version
+          inherit nodejs;
 
-        # Build script to run
-        npmBuildScript = "build";
+          # Build script to run
+          npmBuildScript = "build";
 
-        # Install phase - copy the built site
-        installPhase = ''
-          cp -r dist $out
-        '';
-      };
+          # Install phase - copy the built site
+          installPhase = ''
+            cp -r dist $out
+          '';
+        };
     in {
       packages.site = site;
       packages.default = self.packages.${system}.site;
@@ -64,10 +63,13 @@
 
       apps.serve = {
         type = "app";
-        program = let 
+        program = let
+          inherit (pkgs.lib) getExe;
           server = pkgs.writeShellScriptBin "server" ''
-            ${pkgs.python3}/bin/python3 -m http.server 8080 --directory ${site}""
-          ''; in pkgs.lib.getExe server;
+            ${getExe pkgs.python3} -m http.server 8080 --directory ${site}""
+          '';
+        in
+          getExe server;
       };
     });
 }
