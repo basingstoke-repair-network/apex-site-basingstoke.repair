0b4c640c2df87a3f14fb29a13c317e36ba98e35b
Dom Rodriguez <shymega@shymega.org.uk>
2025-12-06 23:15:34 +0000

    feat: A new start for BRN
    - Initial website for something quick, whilst we work on something
      proper.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ flake.nix
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: 2025 Basingstoke Repair Network <dev@basingstoke.repair>
+#
+# SPDX-License-Identifier: CC0-1.0
+{
+  description = "Astro site for Basingstoke Repair Network";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # Build the site using Nix-native NPM building
+      site = let
+        nodejs = pkgs.nodejs_20;
+      in
+        pkgs.buildNpmPackage {
+          pname = "apex-site-basingstoke-repair";
+          version = "0.1.0";
+
+          src = ./.;
+
+          # Hash of the npm dependencies (you may need to update this)
+          npmDepsHash = import ./deps-hash.nix;
+
+          npmFlags = ["--legacy-peer-deps"];
+
+          # Required for sharp dependency
+          nativeBuildInputs = with pkgs; [
+            python3
+            pkg-config
+            vips
+          ];
+
+          buildInputs = with pkgs; [
+            vips
+          ];
+
+          # Use the specified Node.js version
+          inherit nodejs;
+
+          # Build script to run
+          npmBuildScript = "build";
+
+          # Install phase - copy the built site
+          installPhase = ''
+            cp -r dist $out
+          '';
+        };
+    in {
+      packages.site = site;
+      packages.default = self.packages.${system}.site;
+
+      # Apps for easy running
+      apps.default = self.apps.${system}.serve;
+
+      apps.serve = {
+        type = "app";
+        program = let
+          inherit (pkgs.lib) getExe;
+          server = pkgs.writeShellScriptBin "server" ''
+            ${getExe pkgs.python3} -m http.server 8080 --directory ${site}""
+          '';
+        in
+          getExe server;
+      };
+    });
+}
